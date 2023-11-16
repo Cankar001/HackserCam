04be6410f40c74e3ca23634fd503807b46b6f8e9
Yanneck Gabriel <yanneck.dirk@gmail.com>
2023-11-16 18:05:48 +0100

    color spectrum stub main

---
+++ src/hackser_cam/analyzers/color_spectrum/color_spectrum.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+from .. import analyzer
+import numpy
+
+class color_spectrum (analyzer):
+
+    def __init__(self):
+        #verwendet als variablen deklaration
+        self.img = None
+
+    def run(self, img) -> float:
+        #eigentlicher init
+        self.img = img
+        return 0.0
+
+    def update(self):
+        #refresht für jeden frame
+        print(self.img.shape)
+        pass
--- src/hackser_cam/main.py
+++ src/hackser_cam/main.py
@@ -8,7 +8,7 @@ from .utils import logger as log
 
 from .analyzers.edge_detection import edge_detector
 from .analyzers.greyscale_detection import greyscale_detector
-
+from .analyzers.color_spectrum import color_spectrum
 def load_image(path: Path) -> cv.typing.MatLike:
     img = cv.imread(path)
     assert img is not None, "file could not be read"
@@ -30,6 +30,7 @@ def main(analyzer: str, img_path: click.Path):
         detector = edge_detector()
     elif analyzer == 'color_spectrum':
         log.info('Running color spectrum analysis...')
+        detector = color_spectrum()
     elif analyzer == 'contrast_analyzer':
         log.info('Running contrast analysis...')
     else:
