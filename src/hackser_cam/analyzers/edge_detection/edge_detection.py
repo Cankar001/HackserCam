580530734d2f81e44de0d95433640ed596c935ba
Can Karka <53074877+Cankar001@users.noreply.github.com>
2023-11-16 17:24:59 +0100

    Added empty edge detector

---
+++ src/hackser_cam/analyzers/edge_detection/edge_detection.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+from .. import analyzer
+
+class edge_detector(analyzer):
+    def __init__(self):
+        pass
+
+    def run(self, img) -> float:
+        return 0.0
+    
+    def update(self):
+        pass
